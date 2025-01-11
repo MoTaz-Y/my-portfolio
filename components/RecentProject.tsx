b2b42f5e71b58da4fcb4090bf5e71330e9fcd5c6
MoTaz-Y <motazyasser84@gmail.com>
2025-01-11 15:48:46 +0200

    full project section for now

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -1,6 +1,7 @@
 import { projects } from "@/data";
 import React from "react";
 import { PinContainer } from "./ui/PinContianer";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const RecentProject = () => {
   return (
@@ -9,26 +10,52 @@ const RecentProject = () => {
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex justify-center flex-wrap items-center p-4 gap-8 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24  mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="lg:min-h-[32.5rem] h-[30rem] flex items-center justify-center sm:w-96 w-[80vh] mr-10"
+            className="sm:min-h-[32.5rem] h-[50rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
           >
             <PinContainer title={title} href={link}>
-              <div className="flex flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[22rem] h-[22rem]">
-                <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-                  {title}
-                </h3>
-                <div className="text-base !m-0 !p-0 font-normal">
-                  <span className="text-slate-500">{des}</span>
+              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[20vh] mb-10">
+                <div
+                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                  style={{ backgroundColor: "#13162D" }}
+                >
+                  <img src="/bg.png" alt="bging" className="w-full h-full" />
                 </div>
-                <div className="flex flex-1 w-full rounded-lg mt-4">
-                  <img
-                    src={img}
-                    alt={title}
-                    className="w-full h-full object-cover"
-                  />
+                <img src={img} alt={title} className="z-10 absolute bottom-0" />
+              </div>
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {title}
+              </h1>
+              <p
+                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                style={{
+                  color: "#BEC1DD",
+                  margin: "1vh 0",
+                }}
+              >
+                {des}
+              </p>
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center gap-2">
+                  {iconLists.map((icon, idx) => (
+                    <div
+                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex items-center justify-center "
+                      // style={{ animationDelay: `${idx * 0.1}s` }}
+                      style={{ transform: `translateX(-${5 * idx * 2}px)` }}
+                      key={icon}
+                    >
+                      <img src={icon} alt="icon" className="p-2" />
+                    </div>
+                  ))}
+                </div>
+                <div className="flex items-center gap-2 justify-center">
+                  <p className="flex lg:text-xl md:text-xs test-sm text-purple">
+                    Check my github
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
             </PinContainer>
