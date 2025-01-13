f3c6b4a9bef591654f222155b659246e2ccbf04f
MoTaz-Y <motazyasser84@gmail.com>
2025-01-13 14:25:08 +0200

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,57 @@
 import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
+import { socialMedia } from "@/data";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer
+      className="flex justify-center items-center pb-10 pt-20 w-full"
+      id="contact"
+    >
+      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+        <img
+          src="/footer-grid.svg"
+          alt="footer-grid"
+          className="w-full h-full opacity-50"
+        />
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready to take <span className="text-purple"> YOUR </span> digital
+          presence to the next level{" "}
+        </h1>
+        <p className="text-center text-white-200 md:mt-10 my-5 ">
+          Reach out to me today and let&apso;s bring your vision to life!
+        </p>
+        <a href="mailo:motazyasser84@gmail.com">
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="mt-16 flex md:flex-row flex-col items-center justify-between">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2025 <span className="text-purple"> Motaz</span>
+        </p>
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map(({ id, img, link }) => (
+            <div
+              key={id}
+              className="w-10 h-10 cursor-pointer flex items-center justify-center backdrop-blur-lg backdrop-filter saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <a href={link}>
+                {" "}
+                <img src={img} alt="social-media" width={20} height={20} />
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/MoTaz-Y",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://x.com/motazyasser_y",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://linkedin.com/in/motaz-yasser",
   },
 ];
