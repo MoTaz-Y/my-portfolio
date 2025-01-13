9dcf72e6b2f1ec3e7657a4ed82cfe0c1b9eb995a
MoTaz-Y <motazyasser84@gmail.com>
2025-01-13 10:44:41 +0200

    approach section finished

---
+++ components/Approach.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { CanvasRevealEffect } from "./ui/CanvasRevealEffect";
+import { AceternityIcon, Card } from "./ui/CanvasStuff";
+
+const Approach = () => {
+  return (
+    <section className="w-full py-20">
+      <h1 className="heading">
+        My <span className="text-purple"> Approach </span>
+      </h1>
+      <div className="my-20 flex flex-col lg:flex-row items-center justify-center gap-4">
+        <Card
+          title="Planning & Strategy"
+          icon={<AceternityIcon order="phase 1" />}
+          description="I develop a strategic plan to ensure success in the future. We'll collaborate to map out your website's goals, target audience, 
+          and key functionalities. We'll discuss things like site structure, 
+          navigation, and content requirements."
+        >
+          <CanvasRevealEffect
+            animationSpeed={5.1}
+            containerClassName="bg-emerald-900"
+          />
+        </Card>
+        <Card
+          title="Development & Progress Update"
+          icon={<AceternityIcon order="phase 2" />}
+          description="Once we agree on the plan, I cue my lofi playlist and dive into
+          coding. From initial sketches to polished code, I keep you updated
+          every step of the way."
+        >
+          <CanvasRevealEffect
+            animationSpeed={3}
+            containerClassName="bg-black"
+            colors={[
+              [236, 72, 153],
+              [232, 121, 249],
+            ]}
+            dotSize={2}
+          />
+          {/* Radial gradient for the cute fade */}
+          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
+        </Card>
+        <Card
+          title="Development & Launch"
+          icon={<AceternityIcon order="phase 3" />}
+          description="This is where the magic happens! Based on the approved design, 
+          I'll translate everything into functional code, building your website
+          from the ground up."
+        >
+          <CanvasRevealEffect
+            animationSpeed={3}
+            containerClassName="bg-sky-600"
+            colors={[[125, 211, 252]]}
+          />
+        </Card>
+      </div>
+    </section>
+  );
+};
+
+export default Approach;
